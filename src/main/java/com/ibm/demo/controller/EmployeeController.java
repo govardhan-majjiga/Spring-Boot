47bb20322f1328464e93ba5d62ab565807baa698
govardhan-majjiga <govam8444@gmail.com>
2026-04-07 12:37:54 +0530

    first commit

--- src/main/java/com/ibm/demo/controller/EmployeeController.java
+++ src/main/java/com/ibm/demo/controller/EmployeeController.java
@@ -1,25 +1,93 @@
+
 package com.ibm.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.demo.model.Employee;
+import com.ibm.demo.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
+
+//	private EmployeeService empService = new EmployeeService();
+
+	@Autowired
+	private EmployeeService empService;
+
+	@GetMapping("employees/{id}")
+	public Employee getEmployeeById(@PathVariable(name = "id") int employeeId) {
+		System.out.println(employeeId);
+		return empService.getEmployeeById(employeeId);
+	}
+
 	@GetMapping("employees")
-	public Employee getEmployeeById() {
-		
-		Employee emp =new Employee(1,"Sonu",9000);
-		System.out.println(emp.toString());
-		return emp;	
-		
+	public List<Employee> getAllEmployees() {
+		return empService.getAllEmployees();
 	}
-	@PostMapping("employees")
-	public Employee addEmployee() {
-		return null;
+
+	public Employee addEmployee(Employee employee) {
+		return empService.addEmployee(employee);
 	}
-	
+
+	public Employee updateEmployee(Employee employee) {
+		return empService.updateEmployee(employee);
+	}
+
+	public Employee deleteEmployee(int id) {
+		int employeeId = 2;
+		return empService.deleteEmployee(employeeId);
+	}
 
 }
+
+//package com.ibm.demo.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.ibm.demo.model.Employee;
+//import com.ibm.demo.service.EmployeeService;
+//
+//@RestController
+//public class EmployeeController {
+//
+////	private EmployeeService empService = new EmployeeService();
+//	
+//	@Autowired
+//	private EmployeeService empService;
+//
+//	@GetMapping("employees")
+//	public Employee getEmployeeById() {
+//		int employeeId = 1;
+//		return empService.getEmployeeById(employeeId);
+//	}
+//
+////	@GetMapping("employees")
+////	public List<Employee> getAllEmployees() {
+////		return empService.getAllEmployees();
+////	}
+////
+////	public Employee addEmployee(Employee employee) {
+////		return empService.addEmployee(employee);
+////	}
+////
+////	public Employee updateEmployee(Employee employee) {
+////		return empService.updateEmployee(employee);
+////	}
+////
+////	public Employee deleteEmployee(int id) {
+////		int employeeId = 2;
+////		return empService.deleteEmployee(employeeId);
+////	}
+//
+//}
--- src/main/java/com/ibm/demo/service/EmployeeService.java
+++ src/main/java/com/ibm/demo/service/EmployeeService.java
@@ -1,5 +1,48 @@
+
 package com.ibm.demo.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ibm.demo.model.Employee;
+
+@Service
 public class EmployeeService {
+
+	private List<Employee> employees = new ArrayList<Employee>(Arrays.asList(new Employee(1, "Sonu", 90000),
+			new Employee(2, "Monu", 95000), new Employee(3, "Tonu", 92000), new Employee(4, "Ponu", 94000)));
+
+	public Employee getEmployeeById(int id) {
+		// refactor the code below
+		Employee emp = employees.get(id);
+		System.out.println(emp.toString());
+		return emp;
+	}
+
+	public List<Employee> getAllEmployees() {
+		System.out.println(employees.size());
+		return employees;
+	}
+
+	public Employee addEmployee(Employee employee) {
+		// refactor the code below
+		System.out.println();
+		return null;
+	}
+	
+	public Employee updateEmployee(Employee employee) {
+		// refactor the code below
+		System.out.println();
+		return null;
+	}
+	
+	public Employee deleteEmployee(int id) {
+		// refactor the code below
+		System.out.println();
+		return null;
+	}
 
 }
